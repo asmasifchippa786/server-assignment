5a41ef3283715d0da3689133a13de4c2b73d5e5d
asmasifchippa786 <asmasifchippa786@gmail.com>
2022-12-01 18:26:00 +0500

    fix-all

---
+++ server.mjs
@@ -0,0 +1,25 @@
+import express from 'express';
+import path from 'path'
+const app = express()
+const port = process.env.PORT  || 3000
+
+app.get('/abc', (req, res) => {
+    console.log("requet ip: " , req.ip);
+    res.send('Hello there! I am Here , My Name is M.Anas Asif ' + new Date().toString())
+})
+app.get('/getweather', (req, res) => {
+  console.log("requet ip: " , req.ip);
+  res.send('Hello there! I am Here , My Name is M.Anas Asif ' + new Date().toString())
+})
+app.get('/gettime', (req, res) => {
+  console.log("requet ip: " , req.ip);
+  res.send('Hello there! I am Here , My Name is M.Anas Asif ' + new Date().toString())
+})
+const  __dirname = path.resolve()
+app.use('/', express.static(path.join(__dirname , './server/build')))
+app.get('*', express.static(path.join(__dirname , './server/build')))
+
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
