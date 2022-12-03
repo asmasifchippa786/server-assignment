6cac325e77350b4f15e807bfa24ccc242a82cf96
asmasifchippa786 <asmasifchippa786@gmail.com>
2022-12-03 10:19:53 +0500

    fix

--- server.mjs
+++ server.mjs
@@ -1,25 +1,36 @@
 import express from 'express';
-import path from 'path'
+import path from 'path';
+import cors from 'cors';
+
 const app = express()
-const port = process.env.PORT  || 3000
+const port = process.env.PORT || 5001;
+
+app.use(cors());
 
 app.get('/abc', (req, res) => {
-    console.log("requet ip: " , req.ip);
-    res.send('Hello there! I am Here , My Name is M.Anas Asif ' + new Date().toString())
+    console.log("request ip: ", req.ip);
+    res.send('Hello World! ' + new Date().toString());
 })
-app.get('/getweather', (req, res) => {
-  console.log("requet ip: " , req.ip);
-  res.send('Hello there! I am Here , My Name is M.Anas Asif ' + new Date().toString())
+app.get('/weather', (req, res) => {
+    console.log("request ip: ", req.ip);
+    res.send({
+        temp: 30,
+        min: 26,
+        max: 31,
+        humidity: 72,
+        serverTime: new Date().toString()
+    });
 })
-app.get('/gettime', (req, res) => {
-  console.log("requet ip: " , req.ip);
-  res.send('Hello there! I am Here , My Name is M.Anas Asif ' + new Date().toString())
+app.get('/time', (req, res) => {
+    console.log("request ip: ", req.ip);
+    res.send('Hello World! ' + new Date().toString());
 })
-const  __dirname = path.resolve()
-app.use('/', express.static(path.join(__dirname , './server/build')))
-app.use('*', express.static(path.join(__dirname , './server/build')))
+
+const __dirname = path.resolve();
+app.use('/', express.static(path.join(__dirname, './server/build')))
+app.use('*', express.static(path.join(__dirname, './server/build')))
 
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
+    console.log(`Example app listening on port ${port}`)
 })
--- server/src/App.jsx
+++ server/src/App.jsx
@@ -1,12 +1,57 @@
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-     <h1>I am hello world react front end</h1> 
-    </div>
-  );
+import { useState, useEffect } from "react"
+import axios from "axios";
+
+
+
+
+function Weather() {
+
+    const [weatherData, setWeatherData] = useState(null)
+    const [cityName, setCityName] = useState("")
+
+    const submitHandler = (e) => {
+        e.preventDefault();
+
+        console.log("I am click handler")
+        axios.get(`http://localhost:5001/weather`)
+            .then(response => {
+                console.log("response: ", response.data);
+
+                setWeatherData(response.data);
+            })
+            .catch(err => {
+                console.log("error: ", err);
+            })
+    }
+
+    return (
+        <div>
+
+            <form onSubmit={submitHandler}>
+                City Name:
+                <input type="text" placeholder="enter your city name" required
+                    />
+
+                <button type="submit">get weather</button>
+            </form>
+            <br />
+            <br />
+
+            {(weatherData === null) ? null :
+                <div>
+                    Temperature: {Math.round(weatherData?.temp)}°C
+                    <br />
+                    min: {Math.round(weatherData?.min)}°C
+                    <br />
+                    max: {Math.round(weatherData?.max)}°C
+                </div>
+            }
+        </div>
+    );
 }
 
-export default App;
+export default Weather;
+
+
+
